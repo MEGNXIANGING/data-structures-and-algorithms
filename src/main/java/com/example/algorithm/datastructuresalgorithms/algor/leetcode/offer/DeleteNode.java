a2f07db141176619c51b5d07522213e31c185b6f
guozhengming <1619002088@qq.com>
2020-08-18 10:41:41 +0800

    《剑指offer》 dle node

---
+++ src/main/java/com/example/algorithm/datastructuresalgorithms/algor/leetcode/offer/DeleteNode.java
@@ -0,0 +1,30 @@
+package com.example.algorithm.datastructuresalgorithms.algor.leetcode.offer;
+
+/**
+ * @Author: apple
+ * @Description:
+ * @Date: 2020/8/15 上午11:53
+ */
+public class DeleteNode {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode cur=dummy;
+        while(cur.next!=null){
+            if(cur.next.val==val)cur.next=cur.next.next;
+            else cur=cur.next;
+        }
+        return dummy.next;
+    }
+
+
+}
