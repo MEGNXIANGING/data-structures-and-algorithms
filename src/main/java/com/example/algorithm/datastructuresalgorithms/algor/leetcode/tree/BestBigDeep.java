6b836c2f78f9dfdacd27867f405cb3dfdfcd38dd
guozhengming <1619002088@qq.com>
2020-08-26 14:28:29 +0800

    《剑指offer》二叉树深度

---
+++ src/main/java/com/example/algorithm/datastructuresalgorithms/algor/leetcode/tree/BestBigDeep.java
@@ -0,0 +1,34 @@
+package com.example.algorithm.datastructuresalgorithms.algor.leetcode.tree;
+
+/**
+ * @Author: apple
+ * @Description:
+ * @Date: 2020/8/21 下午5:08
+ */
+public class BestBigDeep {
+
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+     }
+
+    public static void main(String[] args) {
+
+    }
+    public int findRepeatNumber(int[] nums) {
+        int[] temp=new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[nums[i]]++;
+            if (temp[nums[i]]>1){
+                return i;
+            }
+        }
+        return -1;
+
+
+    }
+
+}
