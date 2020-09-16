88067dda13625d3cbf6cc36ef067868b607c9cbd
guozhengming <1619002088@qq.com>
2020-09-16 23:19:13 +0800

    io 基础 - -

--- src/main/java/com/example/algorithm/datastructuresalgorithms/leetcode/arr/TwoNumber.java
+++ src/main/java/com/example/algorithm/datastructuresalgorithms/leetcode/arr/TwoNumber.java
@@ -38,3 +38,28 @@ public class TwoNumber {
         return result;
     }
 }
+
+
+class  huawei{
+    public static void main(String[] args) {
+        method("asdfas","werasdfaswer");
+    }
+    public static void  method(String str1,String str2){
+
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        int index=0;
+        for (char c1:chars1){
+            for (char c2:chars2){
+                if (c1==c2){
+                    index++;
+                }{
+                    index=0;
+                }
+            }
+        }
+        System.out.println(index);
+    }
+}
+
+
