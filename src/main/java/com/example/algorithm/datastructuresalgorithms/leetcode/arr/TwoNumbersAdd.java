395fdf2136235a9d1abb420a643c7cda83b7f42e
guozhengming <1619002088@qq.com>
2020-09-27 09:28:15 +0800

    add two numbers

---
+++ src/main/java/com/example/algorithm/datastructuresalgorithms/leetcode/arr/TwoNumbersAdd.java
@@ -0,0 +1,50 @@
+package com.example.algorithm.datastructuresalgorithms.leetcode.arr;
+
+/**
+ * @Author: apple
+ * @Description: You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * @Date: 2020/9/27 上午8:55
+ */
+public class TwoNumbersAdd {
+    public static void main(String[] args) {
+        System.out.println(13 % 10);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+
+        int addOne = 0;
+        while (l1 != null || l2 != null || addOne != 0) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + addOne;
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+            addOne = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+
+        }
+        return dummy.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
