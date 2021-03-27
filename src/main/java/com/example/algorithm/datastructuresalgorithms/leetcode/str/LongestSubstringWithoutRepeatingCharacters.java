4566ecd3497353683df26fcfe2755214edbc8822
guozhengming <1619002088@qq.com>
2021-03-27 23:37:47 +0800

    longest-substring-without-repeating-characters

---
+++ src/main/java/com/example/algorithm/datastructuresalgorithms/leetcode/str/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,24 @@
+package com.example.algorithm.datastructuresalgorithms.leetcode.str;
+
+/**
+ * @Author: apple
+ * @Description:
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Date: 2021/3/27 下午11:36
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
