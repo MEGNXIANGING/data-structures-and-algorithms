466375c5bc40fedaa5d57d88a8a91da2c75b4de0
mengxianging <1619002088@qq.com>
2021-10-01 21:54:30 +0800

    dp

--- src/main/java/com/example/algorithm/datastructuresalgorithms/leetcode/dp/NumDecodings.java
+++ src/main/java/com/example/algorithm/datastructuresalgorithms/leetcode/dp/NumDecodings.java
@@ -1,7 +1,25 @@
 package com.example.algorithm.datastructuresalgorithms.leetcode.dp;
 
 public class NumDecodings {
-
+    /**
+     * 一条包含字母 A-Z 的消息通过以下映射进行了 编码 ：
+     *
+     * 'A' -> 1
+     * 'B' -> 2
+     * ...
+     * 'Z' -> 26
+     * 要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+     *
+     * "AAJF" ，将消息分组为 (1 1 10 6)
+     * "KJF" ，将消息分组为 (11 10 6)
+     * 注意，消息不能分组为  (1 11 06) ，因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+     *
+     * 给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+     *
+     * 题目数据保证答案肯定是一个 32 位 的整数。
+     * @param ss
+     * @return
+     */
     public int numDecodings(String ss) {
         char[] s = ss.toCharArray();
         int n = s.length;
